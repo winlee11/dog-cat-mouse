c9e9b1391abc9636ae5c7bcb376dc758b3dfbc7f
Charles <trungle15499@gmail.com>
2019-09-24 09:29:35 +0700

    Let dog eat cat

--- Cat.js
+++ Cat.js
@@ -6,4 +6,4 @@ Cat.prototype.eat = function(mouse) {
     this.stomach.push(mouse);
 }; 
 
-module.export = Cat;
+module.exports = Cat;
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var Dog = require('./Dog')
+var Cat = require('./Cat')
 
-var dog = new Dog('Luck')
-dog.sayHi();
+var dog = new Dog('Luck');
+var tom = new Cat();
+dog.eat(tom);
+console.log(dog);
